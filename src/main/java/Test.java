3205317d155c24619769e8266d242bc93155f3f1
gaox <gaoyang123>
2022-08-02 18:05:33 +0800

    测试类

---
+++ src/main/java/Test.java
@@ -0,0 +1,12 @@
+/**
+ * @Description：
+ * @Author：Aaron
+ * @Date：2022-08-02
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        String str = "123456789";
+        System.out.println(str.substring(0, str.length() - 4));
+    }
+}
