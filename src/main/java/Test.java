c1e8356b6e3926a740b95d08b244e197e04773b4
gaox <gaoyang123>
2022-08-03 10:09:12 +0800

    测试类1

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -8,6 +8,7 @@ public class Test {
     public static void main(String[] args) {
         String str = "123456789";
         String str2 = "123456789";
+
         System.out.println(str.substring(0, str.length() - 4));
     }
 }
